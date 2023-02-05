4c80ddb845db60dee99fca90f2a2a725cc3881a4
abdullahmia <abdullah149161@bsdi-bd.org>
2023-02-06 03:23:03 +0600

    :bug: request url encoded

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const router = require('./routes/index');
 
 // middlewares
 app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({extended: true}))
 app.use(morgan('dev'));
 app.use(cors());
 
